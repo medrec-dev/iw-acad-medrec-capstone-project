65d129c69c95e84f9f807036450b5c39f546feda
aaganMaskey <aaganma@gmail.com>
2020-08-17 18:50:38 +0545

    feat: Template initialized

--- root/mainapp/views.py
+++ root/mainapp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+#def home(request):
+    #return HttpResponse("The content is being displayed from [mainapp->view.py]")
+
 def home(request):
-    return HttpResponse("The content is being displayed from [mainapp->view.py]")
+    return render(request,"index.html")
 
 def test(request):
     return HttpResponse("This test is displayed from [mainapp->view.py]")
