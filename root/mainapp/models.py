b8a0aaf585a334943fced09e9f72ca147bac87fc
aaganMaskey <aaganma@gmail.com>
2020-08-19 23:31:46 +0545

    feat: PatientInfo model created and Django Admin initialized

--- root/mainapp/models.py
+++ root/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+class PatientInfo(models.Model):
+    PATIENT_GENDER = (
+        ('F','Female'),
+        ('M', 'Male'),
+        ('O', 'Others')
+    )
+    patient_id = models.AutoField(primary_key=True)
+    patient_firstName = models.CharField(max_length=25)
+    patient_middleName = models.CharField(max_length=25)
+    patient_lastName = models.CharField(max_length=25)
+    patient_email = models.EmailField()
+    patient_dob = models.DateField()
+    patient_gender = models.CharField(max_length=1, choices=PATIENT_GENDER)
 
-# Create your models here.
+def __str__(self):
+    return self.patient_email
