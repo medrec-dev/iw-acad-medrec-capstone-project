ed3d95d4236efa89cab86fe160b8eb002ecfef1a
aaganMaskey <aaganma@gmail.com>
2020-08-24 16:21:15 +0545

    feat: changes in the migration due to model modifications and changed the index

--- root/mainapp/admin.py
+++ root/mainapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import DoctorInfo,PatientInfo,Appointment
-admin.site.register(DoctorInfo)
-admin.site.register(PatientInfo)
+# from .models import Doctor,Patient,Appointment --> OPTIONAL
+from .models import *
+admin.site.register(Doctor)
+admin.site.register(Patient)
 admin.site.register(Appointment)
--- root/mainapp/models.py
+++ root/mainapp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
+from django.db.models.functions import Concat
+#import datetime
+from django.utils import timezone
 
-class DoctorInfo(models.Model):
+#default= 2020-09-11
+class Doctor(models.Model):
     doctor_name = models.CharField(max_length=25)
     doctor_contact = models.IntegerField()
     def __str__(self):
         return self.doctor_name
 
-class PatientInfo(models.Model):
+class Patient(models.Model):
     PATIENT_GENDER = (
         ('F','Female'),
         ('M', 'Male'),
@@ -16,18 +20,22 @@ class PatientInfo(models.Model):
     patient_firstName = models.CharField(max_length=25)
     patient_middleName = models.CharField(max_length=25)
     patient_lastName = models.CharField(max_length=25)
+    #patient_name = models.CharField(max_length=100)
+
+    #patient_name = 'patient_firstName' + 'patient_middleName' + 'patient_lastName'
+    
     patient_email = models.EmailField()
     patient_dob = models.DateField()
     patient_gender = models.CharField(max_length=1, choices=PATIENT_GENDER)
 
     def __str__(self):
-        return self.patient_email #self.patient_firstName
+        return self.patient_email
 
 class Appointment(models.Model):
-    doctor = models.ForeignKey(DoctorInfo,on_delete=models.CASCADE)
-    patient = models.ForeignKey(PatientInfo,on_delete=models.CASCADE)
-    appoint_date = models.DateField
-    appint_time = models.TimeField
-
+    #doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    #patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
+    appoint_date = models.DateTimeField(default=timezone.now)
+    #appoint_time = models.TimeField()
     def __str__(self):
-        return self.doctor.doctor_name + "---" + self.patient.patient_name
+        #return self.doctor.doctor_name + "---" + self.patient.patient_name
+        return self.appoint_date #appoint_time
