418e62e4d26072363136dc97f33365ad32a311b6
aaganMaskey <aaganma@gmail.com>
2020-09-01 12:38:46 +0545

    feat: User registration initialized

---
+++ root/accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, auth
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name)
+    else:
+        return render(request,'register.html')
